98638dd1db3963ce10ddb9793bfe91c68b673226
HannahtenCate <108266121+HannahtenCate@users.noreply.github.com>
2022-11-04 16:56:00 -0400

    Commit initial files

---
+++ index.js
@@ -0,0 +1,32 @@
+let home = 0
+let guest = 0
+
+function homeOne() {
+    home = home + 1
+    document.getElementById("home").textContent = home
+}
+
+function homeTwo() {
+    home = home + 2
+    document.getElementById("home").textContent = home
+}
+
+function homeThree() {
+    home = home + 3
+    document.getElementById("home").textContent = home
+}
+
+function guestOne() {
+    guest = guest + 1
+    document.getElementById("guest").textContent = guest
+}
+
+function guestTwo() {
+    guest = guest + 2
+    document.getElementById("guest").textContent = guest
+}
+
+function guestThree() {
+    guest = guest + 3
+    document.getElementById("guest").textContent = guest
+}
